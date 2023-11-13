4b6e1aff3c77b186a372cef28f7bf6ff5e962385
Veritas93 <loli93@yandex.ru>
2023-11-13 13:58:01 +0300

    Add state machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,8 +2,36 @@ class Task < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :assignee, class_name: 'User', optional: true
 
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :author, presence: true
+  validates :name, :description, :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine initial: :new_task do
+    event :begin_develop do
+      transition new_task: :in_development
+    end
+
+    event :begin_test do
+      transition in_development: :in_qa
+    end
+
+    event :return_in_development do
+      transition [:in_qa, :in_code_review] => :in_development
+    end
+
+    event :start_review do
+      transition in_qa: :code_review
+    end
+
+    event :prepare_for_release do
+      transition code_review: :ready_to_release
+    end
+
+    event :release do
+      transition ready_to_release: :released
+    end
+
+    event :archive do
+      transition [:new_task, :released] => :archived
+    end
+  end
 end
--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     description
     author factory: :manager
     assignee factory: :developer
-    state
+    traits_for_enum(:state, ['new_task', 'in_development', 'in_qa', 'in_code_review', 'ready_for_release', 'released', 'archive'])
     expired_at
   end
 end
