c9fe689ce5f80581e786b1fbc08dd2db2abd8b80
Veritas93 <loli93@yandex.ru>
2024-02-01 17:00:27 +0300

    Authorization for admin

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -2,6 +2,11 @@ include AuthHelper
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionController::TestCase
+	setup do
+		admin = create(:admin)
+		sign_in admin
+	end
+
 	test "should get show" do
 	    user = create(:user)
 	    get :show, params: { id: user.id}
