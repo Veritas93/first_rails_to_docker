e77edf04e1bcdcf0ad1f27cb17b9cda76fa4aadd
Veritas93 <loli93@yandex.ru>
2024-02-05 16:20:31 +0300

    task factories

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :task do
     name
     description
-    author factory: :manager
-    assignee factory: :developer
+    author { create :user }
+    assignee { create :user }
     traits_for_enum(:state,
                     [Task::STATE_NEW_TASK, Task::STATE_IN_DEVELOPMENT, Task::STATE_IN_QA, Task::STATE_IN_CODE_REVIEW,
                      Task::STATE_READY_TO_RELEASE, Task::STATE_RELEASED, Task::STATE_ARCHIVED])
