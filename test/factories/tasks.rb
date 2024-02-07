61f127aa4813c1d2559cb754d8ebce09a6d2ec55
Veritas93 <loli93@yandex.ru>
2024-02-07 11:07:37 +0000

    Add methods to task controller

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :task do
     name
     description
-    author { create :user }
-    assignee { create :user }
+    author factory: :manager
+    assignee factory: :developer
     traits_for_enum(:state,
                     [Task::STATE_NEW_TASK, Task::STATE_IN_DEVELOPMENT, Task::STATE_IN_QA, Task::STATE_IN_CODE_REVIEW,
                      Task::STATE_READY_TO_RELEASE, Task::STATE_RELEASED, Task::STATE_ARCHIVED])
